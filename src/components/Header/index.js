a8180bcd9aed1861377fd2cffe5b10cc99c66ca5
root <rahulkrgupta18032003@gmail.com>
2023-10-26 19:17:12 +0000

    Nxtrendz app authentication 1

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+// Write your JS code here
+import './index.css'
+
+const Header = () => {
+  ;<nav className="nav-header">
+    <div className="nav-content">
+      <div className=""></div>
+    </div>
+  </nav>
+}
+
+export default Header
